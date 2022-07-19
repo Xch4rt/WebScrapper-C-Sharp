dd5326568a0bb988016cb2c56f197eb577779820
Xch4rt <pab203.guti@gmail.com>
2022-07-19 11:59:00 -0600

    Funciona :D

--- WebScraping/Program.cs
+++ WebScraping/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using PuppeteerSharp;
 
-namespace WebScraping
-{
-    internal class Program
+string url = ConfigurationManager.AppSettings["url"];
+string path = ConfigurationManager.AppSettings["chrome_path"];
+
+using var browserFetcher = new BrowserFetcher();
+await browserFetcher.DownloadAsync();
+
+await using var browser = await Puppeteer.LaunchAsync(
+    new LaunchOptions
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Headless = true,
+        ExecutablePath = path,
     }
-}
+    );
+
+await using var page = await browser.NewPageAsync();
+
+
+await page.GoToAsync(url);
+
+List<string> data = new List<string>();
+
+var result = await page.EvaluateFunctionAsync("()=>{"+
+    "const a = document.querySelectorAll('.product-name');" +
+    "const res = [];"+
+    "for (let i = 0; i < a.length; i++)"+
+    "   res.push(a[i].innerHTML);"+
+    "return res;}");
+
+foreach (var item in result)
+    Console.WriteLine(item);
